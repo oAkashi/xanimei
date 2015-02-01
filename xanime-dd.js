f79c4cebcc446bcfba294f650c81b0eaaee0bb83
xAnimeiWebMaster@gmail.com <xAnimeiWebMaster@gmail.com@07677387-d516-1fd7-a7ee-fcfd184b0ed6>
2015-02-01 10:39:24 +0000

    edit

---
+++ xanime-dd.js
@@ -0,0 +1,59 @@
+function mega(IDmegasd,peso,formato) {
+  var output = "<table width=\"550\" border=\"0\" style=\"margin-left:300px;\"><tr><td><a href=\"" + IDmegasd + "\" target=\"_blank\"><img border=\"0\" src=\"http://i.imgur.com/B6Zy8Nr.png\" alt=\"Descargar por Mega SD\" title=\"Descargar por Mega SD\"/></a><b><span style=\"color: white; font-family: Arial, Helvetica, sans-serif; float:left; margin: -15px 60px 0px;\">SD 848x480 " + peso + "MB " + formato + "</span></b>";
+  output += "</td></tr></table>";
+  document.write (output);
+}
+
+function megalite(IDmegalite,peso,formato,pass) {
+  var output = "<table width=\"550\" border=\"0\" style=\"margin-left:300px;\"><tr><td><a href=\"" + IDmegalite + "\" target=\"_blank\"><img border=\"0\" src=\"http://i.imgur.com/B6Zy8Nr.png\" alt=\"Descargar por Mega Ligero\" title=\"Descargar por Mega Ligero\"/></a><b><span style=\"color: white; font-family: Arial, Helvetica, sans-serif; float:left; margin: -15px -37px 0px;\">HD Lite " + peso + " MB " + formato + " Full Color <span style=\"color: red;\">ContraseÃ±a:</span> "+ pass +"</span></b>";
+  output += "</td></tr></table>";
+  document.write (output);
+}
+
+function megahd(IDmegahd,peso,formato,pass) {
+  var output = "<table width=\"550\" border=\"0\" style=\"margin-left:300px;\"><tr><td><a href=\"" + IDmegahd + "\" target=\"_blank\"><img border=\"0\" src=\"http://i.imgur.com/B6Zy8Nr.png\" alt=\"Descargar por Mega HD\" title=\"Descargar por Mega HD\"/></a><b><span style=\"color: white; font-family: Arial, Helvetica, sans-serif; float:left; margin: -15px -45px 0px;\">HD 1280x720 " + peso + " MB " + formato + " Full Color <span style=\"color: red;\">ContraseÃ±a:</span> "+ pass +"</span></b>";
+  output += "</td></tr></table>";
+  document.write (output);
+}
+
+function fic(IDfic,peso,formato,pass) {
+  var output = "<table width=\"550\" border=\"0\" style=\"margin-left:300px;\"><tr><td><a href=\"" + IDfic + "\" target=\"_blank\"><img border=\"0\" src=\"http://i.imgur.com/oFK7iTh.png\" alt=\"Descargar por 1fichier Ligero\" title=\"Descargar por 1fichier Ligero\"/></a><b><span style=\"color: white; font-family: Arial, Helvetica, sans-serif;float:left; margin: -15px -45px 0px;\">HD Lite " + peso + " MB " + formato + " Full Color <span style=\"color: #DDDA00; text-shadow: 1px 1px #A95C03;\">ContraseÃ±a:</span> "+ pass +"</span></b>";
+  output += "</td></tr></table>";
+  document.write (output);
+}
+
+function fichd(IDfic,peso,formato,pass) {
+  var output = "<table width=\"550\" border=\"0\" style=\"margin-left:300px;\"><tr><td><a href=\"" + IDfic + "\" target=\"_blank\"><img border=\"0\" src=\"http://i.imgur.com/oFK7iTh.png\" alt=\"Descargar por 1fichier HD\" title=\"Descargar por 1fichier HD\"/></a><b><span style=\"color: white; font-family: Arial, Helvetica, sans-serif; float:left; margin: -15px -60px 0px;\">HD 1280x720 " + peso + " MB " + formato + " Full Color <span style=\"color: red;\">ContraseÃ±a:</span> "+ pass +"</span></b>";
+  output += "</td></tr></table>";
+  document.write (output);
+}
+
+function sharedlite(IDsharedlite,peso,formato,pass) {
+  var output = "<table width=\"550\" border=\"0\" style=\"margin-left:300px;\"><tr><td><a href=\"" + IDsharedlite + "\" target=\"_blank\"><img border=\"0\" src=\"http://i.imgur.com/YwEraf4.png\" alt=\"Descargar por Shared Ligero\" title=\"Descargar por Shared Ligero\"/></a><b><span style=\"color: white; font-family: Arial, Helvetica, sans-serif; float:left; margin: -15px -45px 0px;\">HD Lite " + peso + " MB " + formato + " Full Color ContraseÃ±a: "+ pass +"</span></b>";
+  output += "</td></tr></table>";
+  document.write (output);
+}
+
+function sharedhd(IDsharedhd,peso,formato,pass) {
+  var output = "<table width=\"550\" border=\"0\" style=\"margin-left:300px;\"><tr><td><a href=\"" + IDsharedhd + "\" target=\"_blank\"><img border=\"0\" src=\"http://i.imgur.com/YwEraf4.png\" alt=\"Descargar por Shared HD\" title=\"Descargar por Shared HD\"/></a><b><span style=\"color: white; font-family: Arial, Helvetica, sans-serif; float:left; margin: -15px -60px 0px;\">HD 1280x720 " + peso + " MB " + formato + " Full Color ContraseÃ±a: "+ pass +"</span></b>";
+  output += "</td></tr></table>";
+  document.write (output);
+}
+
+function uploaded(IDuploaded,peso,formato,pass) {
+  var output = "<table width=\"550\" border=\"0\" style=\"margin-left:300px;\"><tr><td><a href=\"" + IDuploaded + "\" target=\"_blank\"><img border=\"0\" src=\"http://i.imgur.com/ctgoRiC.png\" alt=\"Descargar por Uploaded\" title=\"Descargar por Uploaded Ligero\"/></a><b><span style=\"color: white; font-family: Arial, Helvetica, sans-serif;float:left; margin: -15px -45px 0px;\">HD Lite " + peso + " MB " + formato + " Full Color ContraseÃ±a: "+ pass +"</span></b>";
+  output += "</td></tr></table>";
+  document.write (output);
+}
+
+function uploadedhd(IDsharedhd,peso,formato,pass) {
+  var output = "<table width=\"550\" border=\"0\" style=\"margin-left:300px;\"><tr><td><a href=\"" + IDsharedhd + "\" target=\"_blank\"><img border=\"0\" src=\"http://i.imgur.com/ctgoRiC.png\" alt=\"Descargar por Uploaded HD\ title=\"Descargar por Uploaded HD\"/></a><b><span style=\"color: white; font-family: Arial, Helvetica, sans-serif; float:left; margin: -15px -60px 0px;\">HD 1280x720 " + peso + " MB " + formato + " Full Color ContraseÃ±a: "+ pass +"</span></b>";
+  output += "</td></tr></table>";
+  document.write (output);
+}
+
+function zip(IDzip,peso,formato,pass) {
+  var output = "<table width=\"550\" border=\"0\" style=\"margin-left:300px;\"><tr><td><a href=\"" + IDzip + "\" target=\"_blank\"><img border=\"0\" src=\"http://i.imgur.com/JXerGmn.png\" alt=\"Descargar por Zippyshare\" title=\"Descargar por Zippyshare Ligero\"/></a><b><span style=\"color: white; font-family: Arial, Helvetica, sans-serif;float:left; margin: -15px -45px 0px;\">HD Lite <span style=\"color: #00FF5A; text-shadow: 1px 1px #01740C;\">" + peso + " MB</span> " + formato + " Full Color <span style=\"color: #DDDA00; text-shadow: 1px 1px #A95C03;\">ContraseÃ±a:</span> "+ pass +"</span></b>";
+  output += "</td></tr></table>";
+  document.write (output);
+}
